579e172e8dd4a9bfcd1dc23af0ba7d5d8afbbe91
N1RMAL <83705666+N1RMAL@users.noreply.github.com>
2021-08-18 22:58:23 +0530

    Add files via upload

---
+++ main (8).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+char prog[100], divi[100], name[100], n[100];
+int rollno, i, r;
+int num=0;
+inline int display(const char n[], const int r)
+{
+    num=num+1;
+    system("clear");
+    cout << "Program-" << prog <<"\n"<< "DIVISION-" << divi << "\n";
+    cout << num << ") " << n << "-" << r << "\n\n\n";
+}
+inline int input(const char name[], const int rollno)
+{
+    
+    cout << "Enter the student name:";
+    cin.ignore();
+    cin.getline(n,100);
+    cout << "Enter the roll number:";
+    cin >> r;
+    return display(n, r);
+}
+int main()
+{
+    cout << "Enter the program name:\n";
+    
+    cin >> prog;
+    cout << "Enter the division of the students:\n";
+    cin.ignore();
+    cin >> divi;
+    for(i=1;i<=3;i++)
+    {
+        input(name,rollno);
+    }
+}
